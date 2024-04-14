4c79d7c7189c631ad9194d2b30c4018259598f07
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-14 23:47:00 +0530

    Add files via upload

---
+++ NamePattern.java
@@ -0,0 +1,13 @@
+class NamePattern {
+    public static void main(String[] args) {
+         System.out.println("H");
+         System.out.println("\tA");
+         System.out.println("\t\tR");
+         System.out.println("\t\t\tI");
+
+         System.out.println("12345678");
+         System.out.println("\tHi");
+         System.out.println("Hello\tHi");
+         System.out.println("HelloHRU\tHi");
+    }
+}
